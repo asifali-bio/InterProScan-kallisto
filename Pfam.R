c3e9c29a6d1b20d20242f120cb86ebfca58f0d16
asifali-bio <asifa.github@gmail.com>
2024-10-23 23:28:31 -0500

    Updated

--- Pfam.R
+++ Pfam.R
@@ -4,23 +4,37 @@ library(plyr)
 library(dplyr)
 
 
-#make a list of sample names in CSV format via Excel
+#read CSV containing list of sample/species names
 specieslist = read.csv("specieslist.csv", header = F)
 numberofspecies = nrow(specieslist)
 
+#read kallisto output files as a list
 abundancelist = paste0("abundance", 1:numberofspecies, ".tsv")
 abundancefiles = lapply(abundancelist, read.delim)
-#abundance1 = as.data.frame(abundancefiles[1])
 
+#read InterProScan output files as a list
 annotationlist = paste0("annotation", 1:numberofspecies, ".tsv")
 annotationfiles = lapply(annotationlist, read.delim, header=F)
-#annotation1 = as.data.frame(annotationfiles[1])
+
+#set e-value threshold for Pfam protein domain match
+evalue = 0.001
+
+#filter annotation files by e-value
+filteredannotationfiles <- list()
+for (i in seq(1:numberofspecies)) {
+  filteredannotation = annotationfiles[[i]][which(annotationfiles[[i]]$V9 < evalue),]
+  filteredannotationfiles[specieslist[i,]] <- list(filteredannotation)
+}
+
+filteredannotationfiles[[1]]
+#cycle species by changing the number within double brackets
 
 
 for (i in seq(1:numberofspecies)) {
   
-  justGeneP<-as.data.frame(annotationfiles[i])[,c(1,5)]
-  justGeneTPM<-as.data.frame(abundancefiles[i])[,c(1,5)]
+  #filter Pfam protein domain annotations by e-value
+  justGeneP<-filteredannotationfiles[[i]][,c(1,5)]
+  justGeneTPM<-abundancefiles[[i]][,c(1,5)]
   
   colnames(justGeneP) <- c("gene_id","pfam") 
   
@@ -37,7 +51,7 @@ for (i in seq(1:numberofspecies)) {
   Data3 = Data2
   
   colnames(Data3)[colnames(Data3)=="tpm"] <- specieslist[i,]
-  Data2$newcolumn <- specieslist[i,]
+  Data2$species <- specieslist[i,]
   #label
   
   if (i==1) {
@@ -52,8 +66,11 @@ for (i in seq(1:numberofspecies)) {
   }
 }
 
+#table of pooled protein domains per species filtered by e-value
+new2
 
-p1 = plot_ly(x = new$newcolumn, y = new$pfam, type = "scatter", mode = "markers", color = new$newcolumn, size = new$tpm, fill = ~'', colors = "Spectral")
+#plot of pooled protein domains per species filtered by e-value
+p1 = plot_ly(x = new$species, y = new$pfam, type = "scatter", mode = "markers", color = new$species, size = new$tpm, fill = ~'', colors = "Spectral")
 p1 <- p1 %>% layout(showlegend = FALSE)
 
 p1
@@ -67,45 +84,46 @@ uniquepfam = setNames(
          filter(new2, if_all(setdiff(names(new2[,-1]), x), ~is.na(.)))),
   names(new2[,-1]))
 
-#a=as.data.frame(uniquepfam[1])
-#unique protein domains
+#unique protein domains per species
+#corresponding pooled TPM included
+#filtered by e-value
+uniquepfam[[1]]
+#cycle species by changing the number within double brackets
 
-#annotation = as.data.frame(annotationfiles[1])[which(as.data.frame(annotationfiles[1])$V9 < 0.001),]
-#filter by e-value
 
 finalannotationlist <- list()
 finaltranscriptlist <- list()
 
 for (i in seq(1:numberofspecies)) {
-  a=as.data.frame(uniquepfam[i])
-  annotation = as.data.frame(annotationfiles[i])[which(as.data.frame(annotationfiles[i])$V9 < 0.001),]
+  a=uniquepfam[[i]]
+  filteredannotation = filteredannotationfiles[[i]]
   
   trinity = c()
-  annot = c()
+  annotation = c()
   
   a5 = dim(a)[1]
-  #change letter according to sample
+  #set length of loop
   
   for (j in seq(1:a5)) {
     a3 = a[j,1]
     #grab first unique protein domain entry
-    a1 = grepl(a3, annotation$V5)
+    a1 = grepl(a3, filteredannotation$V5)
     a2 = which(a1)
     #grab position of unique protein domain in filtered data
-    a4 = as.character(annotation[a2,1])
+    a4 = as.character(filteredannotation[a2,1])
     #extract transcript ID
-    a5 = as.character(annotation[a2,5])
+    a5 = as.character(filteredannotation[a2,5])
     #extract Pfam protein domain ID
     trinity = c(trinity, a4)
-    #build data frame of transcripts with unique protein domains filtered by E-value
-    annot = c(annot, a5)
-    #build data frame of unique protein domains filtered by E-value
+    #build data frame of transcripts with unique protein domains filtered by e-value
+    annotation = c(annotation, a5)
+    #build data frame of unique protein domains filtered by e-value
   }
   
-  annot = as.data.frame(annot)
-  annot2 = unique(annot)
+  annotation = as.data.frame(annotation)
+  annotation2 = unique(annotation)
   #unique protein domains
-  colnames(annot2) = specieslist[i,]
+  colnames(annotation2) = specieslist[i,]
   #label species
   
   trinity = as.data.frame(trinity)
@@ -118,13 +136,17 @@ for (i in seq(1:numberofspecies)) {
   #label species
   
   #create a list of annotations and transcripts for each species
-  finalannotationlist[specieslist[i,]] <- list(annot2)
+  finalannotationlist[specieslist[i,]] <- list(annotation2)
   finaltranscriptlist[specieslist[i,]] <- list(trinity2)
 }
 
 
-#a = as.data.frame(finalannotationlist[1])
-#b = as.data.frame(finaltranscriptlist[1])
+#access species-specific information
+#cycle species by changing the number within double brackets
 
+finalannotationlist[[1]]
+#unique protein domains filtered by e-value
 
+finaltranscriptlist[[1]]
+#transcripts with unique protein domains filtered by e-value
 
--- README.md
+++ README.md
@@ -7,4 +7,4 @@ The main purpose of this method is to identify transcripts with a unique protein
 An analogue sequence similarity algorithm has been developed and will be implemented in future updates.
 
 
-To run the function, name the abundance and annotation input files in a similar format as well as the species list CSV file. In R, set the working directory to the same location as the input files. Run the code line-by-line.
+To run the function, name the abundance and annotation input files in a similar format as well as the species list CSV file. In R, set the working directory to the same location as the input files. Run the code line by line.
